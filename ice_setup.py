7ffd15f90c3ded52706e352797bac3d4d3dcd710
Alfredo Deza <alfredo.deza@inktank.com>
2014-01-16 10:28:42 -0500

    handle ICE setup exceptions nicely
    Signed-off-by: Alfredo Deza <alfredo.deza@inktank.com>

--- ice_setup.py
+++ ice_setup.py
@@ -35,6 +35,8 @@ import tarfile
 import tempfile
 import urllib2
 import urlparse
+
+from functools import wraps
 from textwrap import dedent
 
 __version__ = '0.0.1'
@@ -321,6 +323,94 @@ class FileNotFound(ICEError):
 
     def __str__(self):
         return 'could not find %s' % self.filepath
+
+
+# =============================================================================
+# Decorators
+# =============================================================================
+
+def catches(catch=None, handler=None, exit=True):
+    """
+    Very simple decorator that tries any of the exception(s) passed in as
+    a single exception class or tuple (containing multiple ones) returning the
+    exception message and optionally handling the problem if it raises with the
+    handler if it is provided.
+
+    So instead of doing something like this::
+
+        def bar():
+            try:
+                some_call()
+                print "Success!"
+            except TypeError, exc:
+                print "Error while handling some call: %s" % exc
+                sys.exit(1)
+
+    You would need to decorate it like this to have the same effect::
+
+        @catches(TypeError)
+        def bar():
+            some_call()
+            print "Success!"
+
+    If multiple exceptions need to be caught they need to be provided as a
+    tuple::
+
+        @catches((TypeError, AttributeError))
+        def bar():
+            some_call()
+            print "Success!"
+
+    If adding a handler, it should accept a single argument, which would be the
+    exception that was raised, it would look like::
+
+        def my_handler(exc):
+            print 'Handling exception %s' % str(exc)
+            raise SystemExit
+
+        @catches(KeyboardInterrupt, handler=my_handler)
+        def bar():
+            some_call()
+
+    Note that the handler needs to raise its SystemExit if it wants to halt
+    execution, otherwise the decorator would continue as a normal try/except
+    block.
+
+    """
+    catch = catch or Exception
+
+    def decorate(f):
+
+        @wraps(f)
+        def newfunc(*a, **kw):
+            try:
+                f(*a, **kw)
+            except catch as e:
+                if handler:
+                    return handler(e)
+                else:
+                    logger.error(make_exception_message(e))
+                    if exit:
+                        sys.exit(1)
+        return newfunc
+
+    return decorate
+
+#
+# Decorator helpers
+#
+
+
+def make_exception_message(exc):
+    """
+    An exception is passed in and this function
+    returns the proper string depending on the result
+    so it is readable enough.
+    """
+    if str(exc):
+        return '%s: %s' % (exc.__class__.__name__, exc)
+    else:
+        return '%s' % (exc.__class__.__name__)
 
 
 # =============================================================================
@@ -996,6 +1086,7 @@ def sudo_check():
         raise ICEError(msg)
 
 
+@catches(ICEError)
 def main(argv=None):
     # TODO:
     # * add the user prompts for first time runs
@@ -1034,4 +1125,10 @@ def main(argv=None):
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    # This try/except dance *just* for KeyboardInterrupt is horrible but there
+    # is no other way around it if not caught explicitly as opposed to using
+    # the `@catches` decorator. See: http://bugs.python.org/issue1687125
+    try:
+        main()
+    except KeyboardInterrupt:
+        raise SystemExit()
