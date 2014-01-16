b08970f96f9c5301a29c752b7d8eadaaeab0e17a
Alfredo Deza <alfredo.deza@inktank.com>
2014-01-16 11:36:26 -0500

    create some tests for the new helper
    Signed-off-by: Alfredo Deza <alfredo.deza@inktank.com>

---
+++ tests/test_system.py
@@ -0,0 +1,23 @@
+from ice_setup import get_fqdn
+
+
+class FakeSocket(object):
+    pass
+
+
+class TestGetFQDN(object):
+
+    def setup(self):
+        self.sock = FakeSocket()
+
+    def test_dot_local_fqdn(self):
+        self.sock.getfqdn = lambda: 'alfredo.local'
+        assert get_fqdn(_socket=self.sock) is None
+
+    def test_localhost(self):
+        self.sock.getfqdn = lambda: 'localhost'
+        assert get_fqdn(_socket=self.sock) is None
+
+    def test_valid_fqdn(self):
+        self.sock.getfqdn = lambda: 'zombo.com'
+        assert get_fqdn(_socket=self.sock) == 'zombo.com'
