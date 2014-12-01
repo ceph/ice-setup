21cdb414f935bd4a42eeb7ec4ef6ccfafefa944a
Alfredo Deza <alfredo.deza@inktank.com>
2014-12-01 15:37:00 -0500

    update setup.py to use RH
    Signed-off-by: Alfredo Deza <alfredo.deza@inktank.com>

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ICE setup',
-    author='Inktank',
+    name='RHC setup',
+    author='Red Hat',
     version='0.0.1',
     packages=find_packages(),
     zip_safe=False,
