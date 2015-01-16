3036d4762eb80f7241be1177a51ea954cfd25e9b
Ken Dreyer <kdreyer@redhat.com>
2015-01-16 14:07:05 -0700

    packaging: run pytest in "setup.py test"
    Add a helper to setup.py that invokes pytest.
    
    Based on code from http://pytest.org/latest/goodpractises.html
    
    Signed-off-by: Ken Dreyer <kdreyer@redhat.com>

--- setup.py
+++ setup.py
@@ -1,5 +1,25 @@
+import sys
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 from ice_setup.ice import __version__
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 setup(
     name='ice_setup',
@@ -13,4 +33,6 @@ setup(
             'ice_setup = ice_setup.ice:main',
         ],
     },
+    tests_require=['pytest'],
+    cmdclass = {'test': PyTest},
 )
