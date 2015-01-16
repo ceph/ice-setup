4543968d3739b56747355405cd3fd138ee403acd
Ken Dreyer <kdreyer@redhat.com>
2015-01-16 12:24:14 -0700

    packaging: use version number from ice_setup.py
    Avoid hardcoding the ice_setup version number in two places. Make
    setup.py query ice_setup.py for its version number.
    
    Signed-off-by: Ken Dreyer <kdreyer@redhat.com>

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
+from ice_setup import __version__
 
 setup(
     name='ice_setup',
     author='Inktank',
-    version='0.0.1',
+    version=__version__,
     packages=find_packages(),
     zip_safe=False,
 )
