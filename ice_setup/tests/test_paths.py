a5314901f123dac6633bd7e7ece6a4d68805070a
Travis Rhoden <trhoden@redhat.com>
2015-01-28 12:20:33 -0500

    Add tests for package path building
    Unit tests for get_package_source()
    
    Signed-off-by: Travis Rhoden <trhoden@redhat.com>

---
+++ ice_setup/tests/test_paths.py
@@ -0,0 +1,38 @@
+import tempfile
+import os
+import shutil
+
+import pytest
+
+from ice_setup.ice import get_package_source, DirNotFound, VersionNotFound
+
+
+@pytest.fixture
+def pkg_path():
+    pkg_path = tempfile.mkdtemp()
+
+    def fin():
+        shutil.rmtree(pkg_path)
+    return pkg_path
+
+
+class TestGetPackageSource(object):
+
+    def test_unversioned_missing(self, pkg_path):
+        with pytest.raises(DirNotFound):
+            get_package_source(pkg_path, 'ceph')
+
+    def test_unversioned_ok(self, pkg_path):
+        os.mkdir(os.path.join(pkg_path, 'ceph'))
+        path = get_package_source(pkg_path, 'ceph')
+        assert path == os.path.join(pkg_path, 'ceph')
+
+    def test_versioned_missing(self, pkg_path):
+        os.mkdir(os.path.join(pkg_path, 'ceph'))
+        with pytest.raises(VersionNotFound):
+            get_package_source(pkg_path, 'ceph', traverse=True)
+
+    def test_versioned_ok(self, pkg_path):
+        os.makedirs(os.path.join(pkg_path, 'ceph/0.80.0'))
+        path = get_package_source(pkg_path, 'ceph', traverse=True)
+        assert path == os.path.join(pkg_path, 'ceph/0.80.0')
