d75aa008386030f9dd881978ae60c676783824e8
Travis Rhoden <trhoden@redhat.com>
2015-05-20 12:03:31 -0400

    Version 0.4.1
    Signed-off-by: Travis Rhoden <trhoden@redhat.com>

--- ice_setup/ice.py
+++ ice_setup/ice.py
@@ -36,7 +36,7 @@ from ConfigParser import SafeConfigParser, NoSectionError, NoOptionError
 from functools import wraps
 from textwrap import dedent
 
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 
 help_header = """
 
